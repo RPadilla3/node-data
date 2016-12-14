e327dfa7908143b81c4a4a3116c71f09d0f03508
Rodolfo Padilla <Padilla_r95@Rocketmail.com>
2016-12-14 15:40:40 -0500

    Minimized the functions to receive the data fromt he table.

--- app.js
+++ app.js
@@ -1,2 +1,9 @@
-var parsedData = require('./parse');
-var numberOfTickets = parsedData();
+var parse = require('./parse');
+var parsedData = parse();
+var analyze = require('./analyze');
+// var numberOfTickets = parsedData.rawData(function done(data){
+//   console.log('Number of tickets issued equals ' + data.length);
+// });
+var numberOfTickets = analyze().ticketCount(function done(data) {
+  console.log('# of tickets are equal to ' + data.length);
+});
