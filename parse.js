cf7df1a43580b8b82c9711581c9b5cb05347f614
Rodolfo Padilla <Padilla_r95@Rocketmail.com>
2016-12-14 14:35:25 -0500

    Successfully returned the number of tickets for the month of Feb 2016.

--- parse.js
+++ parse.js
@@ -6,20 +6,24 @@ var data = [];
 
 console.log(file);
 
-
-fs.createReadStream('parking_feb_2016.csv').pipe(parse({delimiter: ','}))
+module.exports = function setup() {
+  return fs.createReadStream('parking_feb_2016.csv').pipe(parse({delimiter: ','}))
   .on('data', function(csvrow) {
-    console.log(csvrow);
     data.push(csvrow);
   })
   .on('end', function() {
-    console.log(data);
-  })
+    console.log(data.length);
+    return data.length;
+  });
+
+
+
+};
 
 
 
 //
-//  process.argv.forEach(function findFile(arg) {
+ // process.argv.forEach(function findFile(arg) {
 //     var argPieces = arg.split('/dc');
 //     if (argPieces[1] === '-parking-data-node/simple_data') {
 //         theDataFile = argPieces[1];
