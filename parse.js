f1ce0865d17e9e54d755e1692e5993df454271b1
Rodolfo Padilla <Padilla_r95@Rocketmail.com>
2016-12-14 21:49:27 -0500

    Redid the function that will parse the data into readable information.

--- parse.js
+++ parse.js
@@ -1,36 +1,29 @@
 var fs = require('fs');
 var parse = require('csv-parse');
-var file = 'parking_feb_2016.csv';
 var theDataFile = null;
 
+module.exports = function moduleFunctions(filePath) {
 
-module.exports = function moduleFunctions() {
+    return {
+        rawData: rawData
+    };
 
-  return {
-    rawData: rawData
-  };
+    function rawData(done) {
+        var rawData = [];
+        fs.createReadStream(filePath)
+            .pipe(parse({
+                delimiter: ','
+            }))
+            .on('data', function(csvrow) {
+                rawData.push(csvrow);
+            })
+            .on('end', function() {
+                done(rawData);
+            });
+    }
 
-  function rawData (done) {
-    var rawData = [];
-     fs.createReadStream('parking_feb_2016.csv').pipe(parse({delimiter: ','}))
-     .on('data', function(csvrow) {
-       rawData.push(csvrow);
-     })
-     .on('end', function() {
-        done(rawData);
-     });
-   }
+    // parse('parking_feb_2016.csv', function(err, data) {
+    //   console.log('parsing', data);
+    // });
 
 };
-
-
-
-//
- // process.argv.forEach(function findFile(arg) {
-//     var argPieces = arg.split('/dc');
-//     if (argPieces[1] === '-parking-data-node/simple_data') {
-//         theDataFile = argPieces[1];
-//         console.log(theDataFile);
-//     }
-//     console.log(theDataFile);
-// })
