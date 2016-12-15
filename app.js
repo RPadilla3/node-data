496d653a54d3e7ae6f7e48d2be3ba2530306a283
Rodolfo Padilla <Padilla_r95@Rocketmail.com>
2016-12-14 21:48:34 -0500

    logged out the callback functions to show user the data.

--- app.js
+++ app.js
@@ -1,9 +1,8 @@
-var parse = require('./parse');
-var parsedData = parse();
 var analyze = require('./analyze');
-// var numberOfTickets = parsedData.rawData(function done(data){
-//   console.log('Number of tickets issued equals ' + data.length);
-// });
 var numberOfTickets = analyze().ticketCount(function done(data) {
-  console.log('# of tickets are equal to ' + data.length);
+  console.log('# of tickets are equal to: ' + data.length );
 });
+var violationNumber = analyze().violationTypes(function done(data) {
+  console.log('Number of violation types: ' + data);
+});
+analyze().violationFrequency();
